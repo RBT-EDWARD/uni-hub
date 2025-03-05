78c5b34756e3746188902133c8119fe89ec925f2
RBT-EDWARD <heigor712@gmail.com>
2025-03-05 21:32:15 +0000

    Added API endpoint for Event model and tested API

--- unihub/urls.py
+++ unihub/urls.py
@@ -15,8 +15,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.urls import path, include
 from django.urls import path
+from rest_framework import routers
+from events.api_views import EventViewSet
+
+router = routers.DefaultRouter()
+router.register(r'events', EventViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
 ]
