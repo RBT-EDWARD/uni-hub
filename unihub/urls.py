7bf53ce5d8b22f88fe029c99e752851ce2ace0e6
RBT-EDWARD <heigor712@gmail.com>
2025-03-14 15:27:44 +0000

    error fixing

--- unihub/urls.py
+++ unihub/urls.py
@@ -35,6 +35,8 @@ urlpatterns = [
     path('api/', include(router.urls)),
     path('users/', include('users.urls')),
 
+    path('api/',include('communities.urls'))
+
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
