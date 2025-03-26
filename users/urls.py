2818ee1d07e9a2ea72d07607da5def3a8d71fbfb
Jaspreet <lubana.prince@icloud.com>
2025-03-26 14:53:17 +0530

    UI and Logic of Home, Profile
    1. Added the UI for the Home page, Profile page.
    2. Add the logic of showing profile of logged in user and update profile of logged in user.

--- users/urls.py
+++ users/urls.py
@@ -5,5 +5,8 @@ urlpatterns = [
     path('login/', views.login_view, name='login'),
     path('register/', views.register, name='register'),
     path('logout/', views.logout_view, name='logout'),
-
+    path('index/', views.index_view, name='index'),
+    path('home/', views.home_view, name='home'),
+    path('profile/', views.profile_view, name='profile'),
+    path('update_profile/<int:pk>',views.update_profile,name="update_profile"),
 ]
--- users/views.py
+++ users/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from .forms import UserRegisterForm
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth import logout
+from .models import *
 
 def register(request):
     if request.method == 'POST':
@@ -24,6 +25,35 @@ def login_view(request):
         else:
             return render(request, 'users/login.html', {'error': 'Invalid credentials'})
     return render(request, 'users/login.html')
+
+def index_view(request):
+    return render(request,"users/index_page.html")
+
+def home_view(request):
+    return render(request,"users/home_page.html")
+
+def profile_view(request):
+    print("Logged in user:- ", request.user, request.user.id)
+    logged_in_user_profile = Profile.objects.get(user=request.user)
+    return render(request,"users/profile_page.html",{"profile":logged_in_user_profile})
+
+def update_profile(request,pk):
+    selectedProfile = Profile.objects.get(id=pk)
+    if request.method=="POST":
+        userEmail = request.POST["useremail"]
+        userBio = request.POST["bio"]
+        userInterest = request.POST["interests"]
+
+        selectedProfile.user.email = userEmail
+        selectedProfile.bio = userBio
+        selectedProfile.interests = userInterest
+        selectedProfile.save()
+        selectedProfile.user.save()
+
+        return redirect('profile')
+    else:
+        return redirect('profile')
+
 
 def logout_view(request):
     logout(request)
