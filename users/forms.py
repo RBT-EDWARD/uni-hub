05b57b717406f2a3f5387284f0361cd323d864af
Rajveer <sultandhunna7@gmail.com>
2025-03-27 10:51:18 +0530

    Fix UI bug for the registeration form

--- users/forms.py
+++ users/forms.py
@@ -6,12 +6,11 @@ class UserRegisterForm(UserCreationForm):
     email = forms.EmailField(widget=forms.EmailInput(attrs={"class":"form-control"}))
     bio = forms.CharField(widget=forms.Textarea(attrs={"class":"form-control",'rows':3}), required=False)
     interests = forms.CharField(widget=forms.TextInput(attrs={"class":"form-control",'rows':3}), required=False)
-
+    username =  forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}))
+    password1 = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}))
+    password2 = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}))
+    
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2','bio','interests']
-        widgets  = {
-            'username': forms.TextInput(attrs={'class':'form-control'}),
-            'password1': forms.TextInput(attrs={'class':'form-control'}),
-            'password2': forms.TextInput(attrs={'class':'form-control'})
-        }
+        
