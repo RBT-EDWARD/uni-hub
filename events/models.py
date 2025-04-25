e35c29ffff918f19b53137f65777a1bc89312ef3
RBT-EDWARD <heigor712@gmail.com>
2025-04-25 17:47:06 +0100

    fix the error that can't add new events

--- events/models.py
+++ events/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from communities.models import Community
 from django.contrib.auth.models import User
 
+
 # Create your models here.
 class Event(models.Model):
     title = models.CharField(max_length=100)
@@ -9,6 +10,7 @@ class Event(models.Model):
     date = models.DateTimeField()
     community = models.ForeignKey(Community, on_delete=models.CASCADE)
     participants = models.ManyToManyField(User, related_name='joined_events', blank=True)
+    
 
     def __str__(self):
         return self.title
